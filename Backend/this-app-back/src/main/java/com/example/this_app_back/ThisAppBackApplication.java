a6ab7d183e6e24460eebb99bc126b610276ef32d
mekachi015 <katlegomakoti@gmail.com>
2025-11-24 11:45:13 +0200

    Second commit

---
+++ Backend/this-app-back/src/main/java/com/example/this_app_back/ThisAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.this_app_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisAppBackApplication.class, args);
+	}
+
+}
---
+++ Backend/this-app-back/src/test/java/com/example/this_app_back/ThisAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.this_app_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
